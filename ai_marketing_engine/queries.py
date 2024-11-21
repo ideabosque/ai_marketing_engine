fb9ce4b568855f2922faf96a63bf0dd5d8de64d6
Idea Bosque <ideabosque@gmail.com>
2024-11-21 11:20:25 -0800

    Update queries.

--- ai_marketing_engine/queries.py
+++ ai_marketing_engine/queries.py
@@ -30,85 +30,139 @@ from .handlers import (
     resolve_utm_tag_data_collection_handler,
     resolve_utm_tag_data_collection_list_handler,
 )
+from .types import (
+    CompanyContactProfileListType,
+    CompanyContactProfileType,
+    CompanyCorporationProfileListType,
+    CompanyCorporationProfileType,
+    ContactChatbotHistoryListType,
+    ContactChatbotHistoryType,
+    ContactProfileListType,
+    ContactProfileType,
+    CorporationPlaceListType,
+    CorporationPlaceType,
+    CorporationProfileListType,
+    CorporationProfileType,
+    PlaceListType,
+    PlaceType,
+    QuestionCriteriaListType,
+    QuestionCriteriaType,
+    QuestionListType,
+    QuestionType,
+    UtmTagDataCollectionListType,
+    UtmTagDataCollectionType,
+)
 
 
-def resolve_question(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_question(info: ResolveInfo, **kwargs: Dict[Any, Any]) -> QuestionType:
     return resolve_question_handler(info, **kwargs)
 
 
-def resolve_question_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_question_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> QuestionListType:
     return resolve_question_list_handler(info, **kwargs)
 
 
-def resolve_question_criteria(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_question_criteria(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> QuestionCriteriaType:
     return resolve_question_criteria_handler(info, **kwargs)
 
 
-def resolve_question_criteria_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_question_criteria_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> QuestionCriteriaListType:
     return resolve_question_criteria_list_handler(info, **kwargs)
 
 
-def resolve_place(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_place(info: ResolveInfo, **kwargs: Dict[Any, Any]) -> PlaceType:
     return resolve_place_handler(info, **kwargs)
 
 
-def resolve_place_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_place_list(info: ResolveInfo, **kwargs: Dict[Any, Any]) -> PlaceListType:
     return resolve_place_list_handler(info, **kwargs)
 
 
-def resolve_contact_profile(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_contact_profile(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> ContactProfileType:
     return resolve_contact_profile_handler(info, **kwargs)
 
 
-def resolve_contact_profile_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_contact_profile_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> ContactProfileListType:
     return resolve_contact_profile_list_handler(info, **kwargs)
 
 
-def resolve_company_contact_profile(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_company_contact_profile(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CompanyContactProfileType:
     return resolve_company_contact_profile_handler(info, **kwargs)
 
 
-def resolve_company_contact_profile_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_company_contact_profile_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CompanyContactProfileListType:
     return resolve_company_contact_profile_list_handler(info, **kwargs)
 
 
-def resolve_corporation_profile(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_corporation_profile(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CorporationProfileType:
     return resolve_corporation_profile_handler(info, **kwargs)
 
 
-def resolve_corporation_profile_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_corporation_profile_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CorporationProfileListType:
     return resolve_corporation_profile_list_handler(info, **kwargs)
 
 
-def resolve_company_corporation_profile(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_company_corporation_profile(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CompanyCorporationProfileType:
     return resolve_company_corporation_profile_handler(info, **kwargs)
 
 
 def resolve_company_corporation_profile_list(
     info: ResolveInfo, **kwargs: Dict[Any, Any]
-):
+) -> CompanyCorporationProfileListType:
     return resolve_company_corporation_profile_list_handler(info, **kwargs)
 
 
-def resolve_corporation_place(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_corporation_place(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CorporationPlaceType:
     return resolve_corporation_place_handler(info, **kwargs)
 
 
-def resolve_corporation_place_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_corporation_place_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> CorporationPlaceListType:
     return resolve_corporation_place_list_handler(info, **kwargs)
 
 
-def resolve_contact_chatbot_history(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_contact_chatbot_history(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> ContactChatbotHistoryType:
     return resolve_contact_chatbot_history_handler(info, **kwargs)
 
 
-def resolve_contact_chatbot_history_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_contact_chatbot_history_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> ContactChatbotHistoryListType:
     return resolve_contact_chatbot_history_list_handler(info, **kwargs)
 
 
-def resolve_utm_tag_data_collection(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_utm_tag_data_collection(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> UtmTagDataCollectionType:
     return resolve_utm_tag_data_collection_handler(info, **kwargs)
 
 
-def resolve_utm_tag_data_collection_list(info: ResolveInfo, **kwargs: Dict[Any, Any]):
+def resolve_utm_tag_data_collection_list(
+    info: ResolveInfo, **kwargs: Dict[Any, Any]
+) -> UtmTagDataCollectionListType:
     return resolve_utm_tag_data_collection_list_handler(info, **kwargs)
