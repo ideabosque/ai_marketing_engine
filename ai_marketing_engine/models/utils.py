59d14f0475c84225dfdc81917bec3387b0d2694e
Idea Bosque <ideabosque@gmail.com>
2025-10-09 16:46:36 -0700

    Restructure contact_profile model.

--- ai_marketing_engine/models/utils.py
+++ ai_marketing_engine/models/utils.py
@@ -31,10 +31,18 @@ def _initialize_tables(logger: logging.Logger) -> None:
     return
 
 
-def _get_place(region: str, place_uuid: str) -> Dict[str, Any]:
-    from .place import get_place
+def _get_place(endpoint_id: str, place_uuid: str) -> Dict[str, Any]:
+    from .place import get_place, get_place_count
 
-    place = get_place(region, place_uuid)
+    try:
+        assert (
+            place_uuid is not None
+            and get_place_count(endpoint_id=endpoint_id, place_uuid=place_uuid) == 1
+        ), "Place not found."
+    except AssertionError as e:
+        return {}
+
+    place = get_place(endpoint_id, place_uuid)
     return {
         "region": place.region,
         "place_uuid": place.place_uuid,
@@ -54,7 +62,21 @@ def _get_place(region: str, place_uuid: str) -> Dict[str, Any]:
 
 
 def _get_corporation_profile(endpoint_id: str, corporation_uuid: str) -> Dict[str, Any]:
-    from .corporation_profile import get_corporation_profile
+    from .corporation_profile import (
+        get_corporation_profile,
+        get_corporation_profile_count,
+    )
+
+    try:
+        assert (
+            corporation_uuid is not None
+            and get_corporation_profile_count(
+                endpoint_id=endpoint_id, corporation_uuid=corporation_uuid
+            )
+            == 1
+        ), "Corporation profile not found."
+    except AssertionError as e:
+        return {}
 
     corporation_profile = get_corporation_profile(endpoint_id, corporation_uuid)
     return {
@@ -72,7 +94,10 @@ def _get_contact_profile(endpoint_id: str, contact_uuid: str) -> Dict[str, Any]:
 
     try:
         assert (
-            get_contact_profile_count(endpoint_id=endpoint_id, contact_uuid=contact_uuid)
+            contact_uuid is not None
+            and get_contact_profile_count(
+                endpoint_id=endpoint_id, contact_uuid=contact_uuid
+            )
             == 1
         ), "Contact profile not found."
     except AssertionError as e:
@@ -80,7 +105,6 @@ def _get_contact_profile(endpoint_id: str, contact_uuid: str) -> Dict[str, Any]:
 
     contact_profile = get_contact_profile(endpoint_id, contact_uuid)
     return {
-        "contact_uuid": contact_profile.contact_uuid,
         "place": _get_place(contact_profile.endpoint_id, contact_profile.place_uuid),
         "email": contact_profile.email,
         "first_name": contact_profile.first_name,
