fbc8da15fbc04f6432c2f423e2f237ec104e97d2
Idea Bosque <ideabosque@gmail.com>
2025-11-28 10:34:44 -0800

    refactor: clean up utils.py helper functions

--- ai_marketing_engine/models/utils.py
+++ ai_marketing_engine/models/utils.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 __author__ = "bibow"
 
 import logging
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 from graphene import ResolveInfo
 
@@ -27,80 +27,6 @@ def _initialize_tables(logger: logging.Logger) -> None:
     return
 
 
-def _get_place(endpoint_id: str, place_uuid: str) -> Dict[str, Any]:
-    from .place import get_place, get_place_count
-
-    count = get_place_count(endpoint_id=endpoint_id, place_uuid=place_uuid)
-    if count == 0:
-        return {}
-
-    place = get_place(endpoint_id, place_uuid)
-    return {
-        "region": place.region,
-        "place_uuid": place.place_uuid,
-        "business_name": place.business_name,
-        "latitude": place.latitude,
-        "longitude": place.longitude,
-        "address": place.address,
-        "website": place.website,
-        "types": place.types,
-        "phone_number": place.phone_number,
-        "corporation_profile": (
-            _get_corporation_profile(place.endpoint_id, place.corporation_uuid)
-            if place.corporation_uuid is not None
-            else None
-        ),
-    }
-
-
-def _get_corporation_profile(endpoint_id: str, corporation_uuid: str) -> Dict[str, Any]:
-    from .corporation_profile import (
-        get_corporation_profile,
-        get_corporation_profile_count,
-    )
-
-    count = get_corporation_profile_count(
-        endpoint_id=endpoint_id, corporation_uuid=corporation_uuid
-    )
-    if count == 0:
-        return {}
-
-    corporation_profile = get_corporation_profile(endpoint_id, corporation_uuid)
-    return {
-        "corporation_uuid": corporation_profile.corporation_uuid,
-        "external_id": corporation_profile.external_id,
-        "corporation_type": corporation_profile.corporation_type,
-        "business_name": corporation_profile.business_name,
-        "categories": corporation_profile.categories,
-        "address": corporation_profile.address,
-    }
-
-
-def _get_contact_profile(endpoint_id: str, contact_uuid: str) -> Dict[str, Any]:
-    from .contact_profile import get_contact_profile, get_contact_profile_count
-
-    count = get_contact_profile_count(
-        endpoint_id=endpoint_id, contact_uuid=contact_uuid
-    )
-    if count == 0:
-        return {}
-
-    contact_profile = get_contact_profile(endpoint_id, contact_uuid)
-    return {
-        "endpoint_id": contact_profile.endpoint_id,
-        "contact_uuid": contact_profile.contact_uuid,
-        "email": contact_profile.email,
-        "first_name": contact_profile.first_name,
-        "last_name": contact_profile.last_name,
-        "place_uuid": contact_profile.place_uuid,
-        "place": _get_place(contact_profile.endpoint_id, contact_profile.place_uuid),
-        "data": getattr(contact_profile, "data", None),  # data is optional
-        "updated_by": contact_profile.updated_by,
-        "created_at": contact_profile.created_at,
-        "updated_at": contact_profile.updated_at,
-    }
-
-
 def _insert_update_attribute_values(
     info: ResolveInfo,
     data_type: str,
@@ -108,19 +34,15 @@ def _insert_update_attribute_values(
     updated_by: str,
     data: Dict[str, Any] = {},
 ) -> Dict[str, Any]:
-    from .attribute_value import (
-        insert_update_attribute_values
-    )
+    from .attribute_value import insert_update_attribute_values
+
     params = {
         "data_type": data_type,
         "data_identity": data_identity,
         "data": data,
-        "updated_by": updated_by
+        "updated_by": updated_by,
     }
-    return insert_update_attribute_values(
-        info,
-        **params
-    )
+    return insert_update_attribute_values(info, **params)
 
 
 def _get_data(
